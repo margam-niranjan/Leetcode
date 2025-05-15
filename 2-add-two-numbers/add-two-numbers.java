f1e62e1426913d3f24fa6cffa38eee7e88639290
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-05-16 00:49:58 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (82.77%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -8,25 +8,29 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        int carry = 0;
-        while(l1 != null || l2 != null || carry != 0){
-            int sum = carry;
-            if (l1 != null){
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null){
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            carry = sum/10;
-            curr.next = new ListNode(sum%10);
-            curr = curr.next;
+        return addRecursive(l1, l2, 0);
+    }
+
+    private ListNode addRecursive(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
         }
-        return dummy.next;
+
+        int val1 = (l1 != null) ? l1.val : 0;
+        int val2 = (l2 != null) ? l2.val : 0;
+
+        int sum = val1 + val2 + carry;
+        int newCarry = sum / 10;
+
+        ListNode current = new ListNode(sum % 10);
+        current.next = addRecursive(
+            (l1 != null) ? l1.next : null,
+            (l2 != null) ? l2.next : null,
+            newCarry
+        );
+
+        return current;
     }
 }
