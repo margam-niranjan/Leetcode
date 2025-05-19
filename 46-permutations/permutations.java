94761f12b0b9fd1b03ca39990c50308122c8358b
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-05-19 20:04:32 +0530

    Time: 1 ms (93.58%) | Memory: 44.5 MB (64.03%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -4,24 +4,24 @@ class Solution {
         findPermute(0,nums,res);
         return res;
     }
-    public void findPermute(int ind,int[] nums, List<List<Integer>> res){
-        if(ind==nums.length){
+    public void findPermute(int ind, int[] nums, List<List<Integer>> res){
+        if(ind == nums.length){
             List<Integer> temp = new ArrayList<>();
-            for(int num:nums){
+            for(int num : nums){
                 temp.add(num);
             }
             res.add(new ArrayList<>(temp));
             return;
         }
-        for(int i =ind ; i<nums.length;i++){
-            swap(i,ind,nums);
+        for(int i = ind;i<nums.length;i++){
+            swap(ind,i,nums);
             findPermute(ind+1,nums,res);
-            swap(i,ind,nums);
+            swap(ind,i,nums);
         }
     }
-    public void swap(int i,int ind, int[] arr){
-        int temp = arr[i];
-        arr[i] = arr[ind];
-        arr[ind] = temp;
+    public void swap(int ind,int i,int[] arr){
+        int t = arr[ind];
+        arr[ind] = arr[i];
+        arr[i]=t;
     }
 }
