9ee370209aa320426d5db72c19681f32e4d4b451
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-05-19 19:46:44 +0530

    Time: 1 ms (93.58%) | Memory: 44.7 MB (46.06%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length];
-        findPermute(nums,ds,ans,freq);
-        return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        findPermute(0,nums,res);
+        return res;
     }
-    public void findPermute(int[] nums,List<Integer> ds, List<List<Integer>> ans,boolean[] freq){
-        if(nums.length == ds.size()){
-            ans.add(new ArrayList<>(ds));
+    public void findPermute(int ind,int[] nums, List<List<Integer>> res){
+        if(ind==nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for(int num:nums){
+                temp.add(num);
+            }
+            res.add(new ArrayList<>(temp));
             return;
         }
-        for(int i = 0 ;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]=true;
-                ds.add(nums[i]);
-                findPermute(nums,ds,ans,freq);
-                ds.remove(ds.size()-1);
-                findPermute(nums,ds,ans,freq);
-                freq[i] = false;
-            }
+        for(int i =ind ; i<nums.length;i++){
+            swap(i,ind,nums);
+            findPermute(ind+1,nums,res);
+            swap(i,ind,nums);
         }
+    }
+    public void swap(int i,int ind, int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[ind];
+        arr[ind] = temp;
     }
 }
