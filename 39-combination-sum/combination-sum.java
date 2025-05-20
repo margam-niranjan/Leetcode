3ce27e325210376a78ffabef5f95a6a4883fd41c
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-05-21 01:55:03 +0530

    Time: 2 ms (83.18%) | Memory: 45 MB (21.23%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> can = new ArrayList<>();
+        findComb(nums,0,target,can,res);
+        return res;
+    }
+    public void findComb(int[] nums,int ind,int target,List<Integer> can,List<List<Integer>> res){
+        if(target == 0){
+            res.add(new ArrayList<>(can));
+            return;
+        }
+         if (ind >= nums.length || target < 0) {
+            return;
+        }
+        can.add(nums[ind]);
+        findComb(nums,ind,target-nums[ind],can,res);
+        can.remove(can.size()-1);
+        findComb(nums,ind+1,target,can,res);
+        
+
+    }
+}
