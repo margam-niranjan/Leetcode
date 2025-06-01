7c9605bfd1bfcae2158000b66bd69c40554146ae
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-06-01 19:34:56 +0530

    Time: 2 ms (97.37%) | Memory: 69.7 MB (86.38%) - LeetSync

--- 3732-calculate-score-after-performing-instructions/calculate-score-after-performing-instructions.java
+++ 3732-calculate-score-after-performing-instructions/calculate-score-after-performing-instructions.java
@@ -4,8 +4,6 @@ class Solution {
         long i = 0;
         boolean vis[] = new boolean[values.length];
         while (i < values.length && i>=0 && !vis[(int) i]) {
-            if(i<0 || i>=values.length || vis[(int) i]) 
-                break;
             vis[(int) i] = true;
             if (instructions[(int) i].equals("jump")) {
                 i += values[(int) i];
