5672351852c68070b144084bb23a71493f091a47
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-06-07 01:10:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.30%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -1,40 +1,38 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> res = new ArrayList<>();
-        rootToLeaf(root, res, new StringBuilder());
-        return res;
+        List<String> lst = new ArrayList<>();
+        dfs(root,lst,new StringBuilder());
+        return lst;
     }
-
-    private void rootToLeaf(TreeNode root, List<String> res, StringBuilder sb) {
-        if (root == null) {
-            return;
-        }
+    void dfs(TreeNode root, List<String> res, StringBuilder sb){
+        if(root == null) return;
         int len = sb.length();
         sb.append(root.val);
 
-        if (root.left == null && root.right == null) {
+        if(root.left == null && root.right == null){
             res.add(sb.toString());
-        } else {
+        }
+        else{
             sb.append("->");
-            rootToLeaf(root.left, res, sb);
-            rootToLeaf(root.right, res, sb);
+            dfs(root.left,res,sb);
+            dfs(root.right,res,sb);
         }
         sb.setLength(len);
+        
     }
 }
