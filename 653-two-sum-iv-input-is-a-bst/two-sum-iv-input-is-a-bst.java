6f049efbf262a884f613a4b1eeb39eef42397a00
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-06-08 22:45:37 +0530

    Time: 3 ms (65.85%) | Memory: 44.9 MB (85.33%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,34 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    boolean res = false;
     public boolean findTarget(TreeNode root, int k) {
-        helper(root,root,k);
-        return res;
+        Set<Integer> seen = new HashSet<>();
+        return inorderTraversal(root, k, seen);
     }
-    void helper(TreeNode root,TreeNode node, int k){
-        if(root == null) return;
-        helper2(node, root, k - root.val);
-        helper(root.left,node,k);
-        helper(root.right,node,k);
-    }
-    void helper2(TreeNode root,TreeNode exclude, int k){
-        if(root == null) return ;
-        if(k==root.val && root != exclude) res = true;
-        if(k>root.val) helper2(root.right,exclude,k);
-        if(k<root.val) helper2(root.left,exclude,k);
+
+    private boolean inorderTraversal(TreeNode node, int k, Set<Integer> seen) {
+        if (node == null) {
+            return false;
+        }
+        if (seen.contains(k - node.val)) {
+            return true;
+        }
+        seen.add(node.val);
+        return inorderTraversal(node.left, k, seen) || inorderTraversal(node.right, k, seen);
     }
 }
