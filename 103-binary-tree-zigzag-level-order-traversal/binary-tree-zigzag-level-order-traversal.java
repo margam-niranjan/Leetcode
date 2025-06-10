59168b220a5bd6a0c77ad941afb2b8d325bac9e6
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-06-11 03:55:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (58.37%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,43 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    boolean order = true;
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
-        helper(root,res);
-        return res;
-    }
-    void helper(TreeNode root,List<List<Integer>> res){
+        if (root == null) return res; 
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        while(!q.isEmpty()){
+        boolean order = true; 
+        while (!q.isEmpty()) {
             int size = q.size();
             List<Integer> temp = new ArrayList<>();
-            for(int i = 0 ; i<size; i++){
+            for (int i = 0; i < size; i++) {
                 TreeNode top = q.poll();
-                if(top!=null) temp.add(top.val);
-                if(top.left != null) q.add(top.left);
-                if(top.right != null) q.add(top.right);
+                temp.add(top.val);
+                if (top.left != null) q.add(top.left);
+                if (top.right != null) q.add(top.right);
             }
-            if (!order) Collections.reverse(temp);
+            if (!order) Collections.reverse(temp); 
             res.add(temp);
             order = !order;
-
         }
+        return res;
     }
-    
 }
