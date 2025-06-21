96db6dfd0fad97b2c6b9bff21754ed26407e3551
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-06-21 16:27:43 +0530

    Time: 2 ms (100.00%) | Memory: 42.3 MB (66.99%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
@@ -3,11 +3,7 @@ class Solution {
         int n = words.length, count = 0;
         for(int i = 0 ; i<n;i++){
             for(int j = i+1 ; j<n; j++){
-                int n1 = words[i].length(), n2 = words[j].length();
-                if(n1 <= n2 && words[j].substring(0,n1).equals(words[i]) &&
-                 words[j].substring(n2-n1).equals(words[i])){
-                    count++;
-                }
+                if(words[j].startsWith(words[i]) && words[j].endsWith(words[i])) count++;
             }
         }
         return count;
