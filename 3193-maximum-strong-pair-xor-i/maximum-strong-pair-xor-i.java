0863d9fbb79da08bb565a1fa8c591ced001002a8
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-06-21 13:54:08 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (75.90%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.java
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.java
@@ -3,8 +3,10 @@ class Solution {
         int max = -1;
         for(int i = 0 ; i<nums.length; i++){
             for(int j = i ; j<nums.length ; j++){
-                 if (Math.abs(nums[i] - nums[j]) <= Math.min(nums[i], nums[j]))
-                max = Math.max(nums[i] ^ nums[j],max);
+                if((nums[i]^nums[j])>max && Math.abs(nums[i]-nums[j])<=Math.min(nums[i],nums[j]))
+                {
+                    max=nums[i]^nums[j];
+                }
             }
         }
         return max;
