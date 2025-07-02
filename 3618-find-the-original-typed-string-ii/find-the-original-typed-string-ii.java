26fa53cb0d0c140be11cde477b2175a758718d25
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-07-02 22:23:25 +0530

    Time: 171 ms (55.56%) | Memory: 57 MB (13.89%) - LeetSync

---
+++ 3618-find-the-original-typed-string-ii/find-the-original-typed-string-ii.java
@@ -0,0 +1,53 @@
+class Solution {
+
+    private static final int mod = 1000000007;
+
+    public int possibleStringCount(String word, int k) {
+        int n = word.length();
+        int currentCount = 1;
+        List<Integer> groupSizes = new ArrayList<>();
+        for (int i = 1; i < n; ++i) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                currentCount++;
+            } else {
+                groupSizes.add(currentCount);
+                currentCount = 1;
+            }
+        }
+        groupSizes.add(currentCount);
+        long totalWays = 1;
+        for (int size : groupSizes) {
+            totalWays = (totalWays * size) % mod;
+        }
+        if (groupSizes.size() >= k) {
+            return (int) totalWays;
+        }
+        int[] dp = new int[k]; 
+        int[] prefixSum = new int[k];
+
+        dp[0] = 1;
+        Arrays.fill(prefixSum, 1);
+
+        // Har group process karo
+        for (int i = 0; i < groupSizes.size(); ++i) {
+            int groupSize = groupSizes.get(i);
+            int[] newDp = new int[k];
+
+            for (int j = 1; j < k; ++j) {
+                newDp[j] = prefixSum[j - 1];
+                if (j - groupSize - 1 >= 0) {
+                    newDp[j] = (newDp[j] - prefixSum[j - groupSize - 1] + mod) % mod;
+                }
+            }
+            int[] newPrefix = new int[k];
+            newPrefix[0] = newDp[0];
+            for (int j = 1; j < k; ++j) {
+                newPrefix[j] = (newPrefix[j - 1] + newDp[j]) % mod;
+            }
+
+            dp = newDp;
+            prefixSum = newPrefix;
+        }
+        return (int) ((totalWays - prefixSum[k - 1] + mod) % mod);
+    }
+}
