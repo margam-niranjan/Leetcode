69a903839b3472c0317e48c9ef5a036b0710d152
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-07-25 07:24:38 +0530

    Time: 2 ms (96.65%) | Memory: 41.8 MB (65.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stk = new Stack<>();
-        if(s.length() %2 !=0)return false;
-        for(char ch:s.toCharArray()){
-            if(ch=='(' || ch=='[' || ch=='{'){
-                stk.push(ch);
+        if(s.length() % 2 != 0) return false;
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '[')stk.push(ch);
+            else if(!stk.isEmpty() && match(stk.peek(),ch)) {
+                stk.pop();
             }
-            else {
-                if (stk.isEmpty()) return false;
-                char top = stk.peek();
-                if ((ch == ')' && top == '(') ||
-                    (ch == ']' && top == '[') ||
-                    (ch == '}' && top == '{')) {
-                    stk.pop();
-                } 
-                else {
-                    return false;
-                }
-            }
-        } 
-        return stk.size()==0;
+            else return false;
+        }
+        return stk.isEmpty();
+    }
+    boolean match(char ch1, char ch2){
+        if(ch1 == '(' && ch2 == ')') return true;
+        if(ch1 == '{' && ch2 == '}') return true;
+        if(ch1 == '[' && ch2 == ']') return true;
+        return false;
     }
 }
