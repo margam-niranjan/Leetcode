493efc34eaf1f76a272ecaa541a16b8cf080e473
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-08-02 19:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (26.29%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -6,11 +6,10 @@ class Solution(object):
         prefLen = len(pref)
         for i in range(1,len(strs)):
             s = strs[i]
-            while prefLen > len(s) or  s[0:prefLen] != pref:
+            while prefLen > len(s) or s[0:prefLen] != pref:
                 prefLen = prefLen - 1
                 if not pref:
                     return ""
                 pref = pref[0:prefLen]
         return pref
-        # java loo cheppochaa???? expain chesta every term java looo okay aaa??? vachaaka cheppu
-        # cheppu kani aag 
+        
