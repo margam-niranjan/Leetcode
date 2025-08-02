05af6376de0dd0531d6d4c0b356c13eeec496023
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-08-02 22:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.42%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,15 +1,7 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        p = (m - 1) 
-        q = (n - 1) 
-        r = m+n -1
-        while(q>=0):
-            if(p>=0 and nums1[p] > nums2[q]):
-                nums1[r] = nums1[p]
-                r = r-1
-                p = p-1
-            else:
-                nums1[r] = nums2[q]
-                r = r-1
-                q = q-1
+        for i in range(n):
+            nums1[m+i]=nums2[i]
+        nums1.sort()
+        
         
