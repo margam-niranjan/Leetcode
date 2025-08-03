a3d468498b63257db539e2ca209b36df39becbaf
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-08-03 06:28:57 +0530

    Time: 7 ms (43.45%) | Memory: 14.1 MB (31.42%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def singleNumber(self, nums):
-        a=set(nums)
-        for i in a:
-            if nums.count(i)==1:
-                return i
+        ans = 0
+        for i in range(len(nums)):
+            ans = ans ^ nums[i]
+        return ans
