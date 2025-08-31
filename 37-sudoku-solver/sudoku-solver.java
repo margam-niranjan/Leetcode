e331fd058a02db96057ec42a2f15b638832a9095
Niranjan Margam <81058613+margam-niranjan@users.noreply.github.com>
2025-09-01 02:04:53 +0530

    Time: 41 ms (98.76%) | Memory: 41.3 MB (44.11%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,30 +1,37 @@
 class Solution {
+    private boolean[][] rows = new boolean[9][9];
+    private boolean[][] cols = new boolean[9][9];
+    private boolean[][] boxes = new boolean[9][9];
+
     public void solveSudoku(char[][] board) {
-        solve(board);
-    }
-    boolean solve(char[][] board){
-        for(int i = 0 ; i<9; i++){
-            for(int j = 0 ; j<9 ; j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c<='9' ; c++){
-                        if(isValid(board, i, j, c)){
-                            board[i][j] = c;
-                            if(solve(board))return true;
-                            else board[i][j] = '.';
-                        }
-                    }
-                    return false;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    rows[i][num] = true;
+                    cols[j][num] = true;
+                    boxes[boxIndex(i, j)][num] = true;
                 }
             }
         }
-        return true;
+        solve(board, 0, 0);
     }
-    boolean isValid(char[][] board,int row, int col, char ch){
-        for(int i = 0 ; i<9; i++){
-            if(board[i][col] == ch) return false;
-            if(board[row][i] == ch) return false;
-            if(board[3 * (row/3) + i / 3][3 * (col/3) + i%3] == ch) return false;
+    private boolean solve(char[][] board, int row, int col) {
+        if (row == 9) return true;
+        if (col == 9) return solve(board, row + 1, 0);
+        if (board[row][col] != '.') return solve(board, row, col + 1);
+        for (int num = 0; num < 9; num++) {
+            if (!rows[row][num] && !cols[col][num] && !boxes[boxIndex(row, col)][num]) {
+                board[row][col] = (char)(num + '1');
+                rows[row][num] = cols[col][num] = boxes[boxIndex(row, col)][num] = true;
+                if (solve(board, row, col + 1)) return true;
+                board[row][col] = '.';
+                rows[row][num] = cols[col][num] = boxes[boxIndex(row, col)][num] = false;
+            }
         }
-        return true;
+        return false;
+    }
+    private int boxIndex(int row, int col) {
+        return (row / 3) * 3 + col / 3;
     }
 }
